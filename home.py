b1503a1d525615bf467d270011f48ac42268e91a
John Pelletier <john.pelletier@ymail.com>
2020-05-18 15:41:05 -1000

    adding server query to images to avoid bad cacheing

--- home.py
+++ home.py
@@ -1,11 +1,18 @@
 import json
+import time
 from flask import Flask, render_template
 
 def home():
     title = 'Home'
     datadic = {}
+    now = time.time()
+    dstring = '/static/images/'
+    qstring = '?' + str(time.time())
+    piclist = [dstring + 'Light.jpeg'+qstring,
+               dstring + 'Hum.jpeg'+qstring,
+               dstring + 'Temps.jpeg'+qstring]
     with open("datadic.txt") as f:
         for line in f:
            (key, val) = line.split()
            datadic[key] = val
-    return render_template('maindisplay.html', title=title, datadic=datadic)
+    return render_template('maindisplay.html', title=title, datadic=datadic, piclist=piclist)
